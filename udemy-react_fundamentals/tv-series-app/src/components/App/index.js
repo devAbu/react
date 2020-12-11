54bb94e1051d7ad141f0220fda9345e154c3392a
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-12-11 15:33:22 +0100

    Container

--- udemy-react_fundamentals/tv-series-app/src/components/App/index.js
+++ udemy-react_fundamentals/tv-series-app/src/components/App/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import { Component } from "react";
 import Intro from "../Intro";
+import Series from "../../containers/Series";
 import "whatwg-fetch"; //send request
 
 /* const Intro = (props) => {
@@ -11,29 +12,6 @@ import "whatwg-fetch"; //send request
 } */
 
 class App extends Component {
-  state = {
-    series: [],
-  };
-
-  /* Automatically invoked after the component has been rendered  */
-  componentDidMount() {
-    /* Anything can be *
-    const series = ["Vikings", "Game of Thrones"];
-
-    setTimeout(() => {
-      /* set state - tells react that this component and its children need to be re-rendered with the updated state*
-      this.setState({ series });
-      /* {series: series } -- modern js = {series} 
-    }, 2000);*/
-
-    /* Promises = .then */
-    fetch("http://api.tvmaze.com/search/shows?q=Vikings")
-      .then((res) => {
-        /* console.log(res); */
-        return res.json();
-      })
-      .then((res) => this.setState({ series: res }));
-  }
   render() {
     return (
       <div className="App">
@@ -41,7 +19,7 @@ class App extends Component {
           <h1>TV SERIES APP</h1>
         </header>
         <Intro message="Here you can find all of your most loved series" />
-        The length of series array: {this.state.series.length}
+        <Series />
       </div>
     );
   }
---
+++ udemy-react_fundamentals/tv-series-app/src/containers/Series/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Series extends Component {
+  state = {
+    series: [],
+  };
+
+  /* Automatically invoked after the component has been rendered  */
+  componentDidMount() {
+    /* Anything can be *
+    const series = ["Vikings", "Game of Thrones"];
+
+    setTimeout(() => {
+      /* set state - tells react that this component and its children need to be re-rendered with the updated state*
+      this.setState({ series });
+      /* {series: series } -- modern js = {series} 
+    }, 2000);*/
+
+    /* Promises = .then */
+    fetch("http://api.tvmaze.com/search/shows?q=Vikings")
+      .then((res) => {
+        /* console.log(res); */
+        return res.json();
+      })
+      .then((res) => this.setState({ series: res }));
+  }
+  render() {
+    return <div> The length of series array: {this.state.series.length}</div>;
+  }
+}
+
+export default Series;
