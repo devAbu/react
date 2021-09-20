657e4178cb660c17561553203d755fb94417ce39
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-09-20 13:19:01 +0200

    React basics

---
+++ react-maxi-yt/react-course/src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button className="btn btn--alt">Confirm</button>
+      <button className="btn">Cancel</button>
+    </div>
+  );
+}
+
+export default Modal;
