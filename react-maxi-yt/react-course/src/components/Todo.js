29adc77893ec69399f474614e5acb3c196b01687
devAbu <abud_almonajed@hotmail.com>
2022-06-23 18:36:21 +0200

    State - Intro

--- react-maxi-yt/react-course/src/components/Todo.js
+++ react-maxi-yt/react-course/src/components/Todo.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false); // value OR variable, function that allows to change the value
   function deleteHandler() {
     /* console.log("deleteHandler called");
     console.log(props.title); */
+    setModalIsOpen(true); // change the state from false to true
   }
 
   return (
@@ -12,6 +19,8 @@ function Todo(props) {
           DELETE
         </button>
       </div>
+      {modalIsOpen ? <Modal /> : null}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
