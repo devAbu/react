561d5a6fedca2c589777e2ea184a3da4f64f4300
devAbu <abud_almonajed@hotmail.com>
2022-07-01 19:18:10 +0200

    Rout basics

--- react-maxi-yt/meet-up/src/index.js
+++ react-maxi-yt/meet-up/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 /* import ReactDOM from "react-dom"; */
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 // React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 /* ReactDOM.render(<App />, document.getElementById("root")); */
