35aae67abba4b06f4df4b638182b62c7b5963edb
devAbu <abud_almonajed@hotmail.com>
2022-12-26 16:18:49 +0100

    ...

--- maxi-react-yt/adv-course/src/App.js
+++ maxi-react-yt/adv-course/src/App.js
@@ -2,17 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favorite from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<AllMeetups />}></Route>
-        <Route path="/new-meetup" element={<NewMeetup />}></Route>
-        <Route path="/favorite" element={<Favorite />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetups />}></Route>
+          <Route path="/new-meetup" element={<NewMeetup />}></Route>
+          <Route path="/favorite" element={<Favorite />}></Route>
+        </Routes>
+      </Layout>
     </div>
   );
 }
--- maxi-react-yt/adv-course/src/pages/NewMeetup.js
+++ maxi-react-yt/adv-course/src/pages/NewMeetup.js
@@ -1,3 +1,10 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 export default function NewMeetup() {
-  return <div>New Meetup</div>;
+  return (
+    <section>
+      <h1>New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
