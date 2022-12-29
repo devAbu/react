49af34bbc0cdf6564286e1dc5e0c50c229b8f8d1
devAbu <abud_almonajed@hotmail.com>
2022-12-29 14:46:31 +0100

    Submit to DB - Firebase

--- maxi-react-yt/adv-course/src/pages/NewMeetup.js
+++ maxi-react-yt/adv-course/src/pages/NewMeetup.js
@@ -1,10 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 export default function NewMeetup() {
+  const history = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-9aa94-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(() => {
+      history("/");
+    });
+  }
+
   return (
     <section>
       <h1>New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
